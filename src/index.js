f3f669d1f32117e3d096621960c8bef31630a671
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-09-17 18:36:36 +0800

    add routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.on('open', () => console.log('connected to database'));
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('hello from server');
+});
+
+app.post('/', (req, res) => {
+    res.send('hello from server');
+});
+
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('hello from server');
+});
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('hello from server');
+});
 
 app.listen(8080, () => {
-    'server listening on port 8080'
+    console.log('server listening on port 8080');
 });
